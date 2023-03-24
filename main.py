52882298bc1e3702f6424892650c88d144fc2606
Benjamin2her <82844373+Benjamin2her@users.noreply.github.com>
2023-03-23 20:46:20 -0700

    si
    no se, ayuda

---
+++ main.py
@@ -0,0 +1,7 @@
+from simulated_annealing import Log
+
+def main():
+    Log("Hello world")
+
+if __name__ == "__main__":
+    main()
---
+++ simulated_annealing.py
@@ -0,0 +1,17 @@
+from math import pow
+
+def metropolis_montecarlo():
+    # 1.- Random configuration s and compute E(s)
+    # 2.- Random mutation s' of s and Compute E(s')
+    # 3.- Accept the new configuration s' with probability:
+    #   P = {
+    # 
+    # 
+    # }
+    pass
+
+def simulated_annealing(initial_solution, initial_temperature):
+    terminate = False
+    while not terminate:
+        pass
+    pass
